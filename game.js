369daf83bcf908b39bf6998722205dcb0f0034d7
benwheels <wheelhousebenjamin@gmail.com>
2021-02-01 01:36:50 -0800

    player in fog

--- game.js
+++ game.js
@@ -1,10 +1,34 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-drawPlayer();
+var w = canvas.width;
+var h = canvas.height;
 
-function drawPlayer() {
-    ctx.rect(64,64,50,50);
+var x = 65
+var y = 65
+
+var p_size = 10;
+var p
+
+drawPlayer();
+drawKey();
+drawFog();
+
+function drawPlayer() { 
     ctx.fillStyle = 'red';
+    ctx.fillRect(x,y,10,10);
+}
+
+function drawKey() {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(540,230,10,10);
+}
+
+
+function drawFog() {
+    ctx.fillStyle = "grey";
+    ctx.beginPath();
+    ctx.arc(x+5, y+5, 28, 0, 2 * Math.PI);
+    ctx.rect(640, 0, -640, 480);
     ctx.fill();
 }

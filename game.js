1ca12d3b82dcccd3946bff5672636ac9d11e81e3
matti <lime811@gmail.com>
2021-02-02 20:10:49 -0800

    key now looks like a real key. added collision detection.

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
 class Player {
-    constructor(size,x,y,v,hue) {
+    constructor(size,x,y,hue) {
         this.size = size;
         this.x = x;
         this.y = y;
-        this.v = v;
         this.hue = hue;
     }
     draw() {
-        this.move();
-        this.vision();
+        this.vision()
         ctx.fillStyle = this.hue;
         ctx.fillRect(this.x,this.y,10,10);
     }
@@ -16,39 +14,77 @@ class Player {
         ctx.arc(this.x+5,this.y+5,55,0,2 * Math.PI);
         ctx.closePath();
     }
-    move() {
-        requestAnimationFrame(this.move, 42)
-        if (keys[38]) {
-            this.y -= this.v;
+
+}
+
+class Key {
+
+    held = false;
+
+    constructor(size,x,y,dx,dy) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+    }
+    draw() {
+
+        // fill
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(this.x,this.y,this.size,this.size);
+
+        // erase
+        ctx.globalCompositeOperation='destination-out';
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x+this.size/6,this.y+this.size/6,this.size/1.5,this.size/1.5)
+
+        // fill
+        ctx.globalCompositeOperation='source-over';
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(this.x+this.size,this.y+this.size/2,this.size,this.size/8)
+
+        // fill
+        ctx.fillRect(this.x+this.size*2,this.y+this.size/2,this.size/6,this.size/3);
+        ctx.fillRect(this.x+this.size*1.5,this.y+this.size/2,this.size/6,this.size/3);
+
+    }
+    collide_check(player, key) {
+        if(this.held === false) {
+            check = collision(player, key);
+            if(check === true) {
+                this.held = true;
+                this.x = player.x;
+                this.y = player.y;
+            }
         }
+
     }
 
 }
 
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
-
-canvas.width = 800;
-canvas.height = 600;
-
-var keys = [];
-
-let player = new Player(10,64,64,5,'red');
-let player2 = new Player(10, 478, 234,5,'blue');
-let player3 = new Player(10, 120, 460,5,'yellow');
-//let key = new Key();
-
-player.draw();
-player2.draw();
-player3.draw();
-
-ctx.fillStyle = "grey";
-ctx.rect(canvas.width,0,-canvas.width,canvas.height);
-ctx.fill();
-
-function draw() {
-    
+function collision(rect1, rect2) {
+    if (rect1.x < rect2.x + rect2.size &&
+        rect1.x + rect1.size > rect2.x &&
+        rect1.y < rect2.y + rect2.size &&
+        rect1.y + rect1.size > rect2.y) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-setInterval(draw, 42);
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight-200;
 
+let player = new Player(20,64,64,'red');
+let key = new Key(10, 200, 200);
+
+player.draw();
+key.draw();
+
+//ctx.fillStyle = "grey";
+//ctx.rect(canvas.width,0,-canvas.width,canvas.height);
+//ctx.fill();

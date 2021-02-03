9fd2415d271c9856c099fd68f99ea08d1fec05a4
benwheels <wheelhousebenjamin@gmail.com>
2021-02-02 18:32:21 -0800

    working on movement

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 class Player {
-    constructor(size,x,y,hue) {
+    constructor(size,x,y,v,hue) {
         this.size = size;
         this.x = x;
         this.y = y;
+        this.v = v;
         this.hue = hue;
     }
     draw() {
-        this.vision()
+        this.move();
+        this.vision();
         ctx.fillStyle = this.hue;
         ctx.fillRect(this.x,this.y,10,10);
     }
@@ -14,18 +16,26 @@ class Player {
         ctx.arc(this.x+5,this.y+5,55,0,2 * Math.PI);
         ctx.closePath();
     }
+    move() {
+        requestAnimationFrame(this.move, 42)
+        if (keys[38]) {
+            this.y -= this.v;
+        }
+    }
 
 }
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 800;
+canvas.height = 600;
 
-let player = new Player(10,64,64,'red');
-let player2 = new Player(10, 478, 234,'blue');
-let player3 = new Player(10, 120, 460,'yellow');
+var keys = [];
+
+let player = new Player(10,64,64,5,'red');
+let player2 = new Player(10, 478, 234,5,'blue');
+let player3 = new Player(10, 120, 460,5,'yellow');
 //let key = new Key();
 
 player.draw();
@@ -35,3 +45,10 @@ player3.draw();
 ctx.fillStyle = "grey";
 ctx.rect(canvas.width,0,-canvas.width,canvas.height);
 ctx.fill();
+
+function draw() {
+    
+}
+
+setInterval(draw, 42);
+

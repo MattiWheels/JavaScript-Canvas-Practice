19db9e7784b7cedcc5ef2ffb530c0a9f0a4cd022
benwheels <wheelhousebenjamin@gmail.com>
2021-02-02 23:08:17 -0800

    movement stage 1

--- game.js
+++ game.js
@@ -17,13 +17,12 @@ class Player {
         ctx.arc(this.x+5,this.y+5,55,0,2 * Math.PI);
         ctx.closePath();
     }
-    move() {
-        
-    }
 
 }
 
-function keyDownHandler(e) {
+//previous movement functions temporarily commented out.
+
+//function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
         console.log('right');
@@ -46,17 +45,26 @@ function keyDownHandler(e) {
     }
 }
 
-function keyUpHandler(e) {
+//function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = false;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = false;
+    }
 }
 
+//main loop.
 function update() {
     requestAnimationFrame(update);
+
+    
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
     player.draw();
@@ -73,13 +81,22 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+var keys = [];
 
-let player = new Player(10,64,64,10,'red');
+//document.addEventListener("keydown", keyDownHandler, false);
+//document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keydown", function(e) {
+    keys[e.key] = true;
+});
+document.addEventListener("keyup", function(e) {
+    keys[e.key] = false;
+});
+
+
+let player = new Player(10,64,64,1,'red');
 let player2 = new Player(10, 478, 234,5,'blue');
 let player3 = new Player(10, 120, 460,5,'yellow');
 //let key = new Key();
 
-requestAnimationFrame(update);
+update();
 //myVar = setInterval(update, 42);

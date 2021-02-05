094ee7ba707b9614d17b1e6cae80191860c52d0c
matti <lime811@gmail.com>
2021-02-05 00:46:23 -0800

    comments

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ class Key {
         ctx.globalCompositeOperation='destination-out';
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.rect(this.x+this.size/6,this.y+this.size/6,this.size/1.5,this.size/1.5)
+        ctx.rect(this.x+this.size/6,this.y+this.size/6,this.size/1.5,this.size/1.5);
         ctx.closePath();
         ctx.fill();
 
@@ -67,7 +67,7 @@ class Key {
         ctx.globalCompositeOperation='source-over';
         ctx.fillStyle = 'yellow';
         ctx.beginPath();
-        ctx.rect(this.x+this.size,this.y+this.size/2,this.size,this.size/8)
+        ctx.rect(this.x+this.size,this.y+this.size/2,this.size,this.size/8);
         ctx.closePath();
         ctx.fill();
 
@@ -80,10 +80,10 @@ class Key {
 
     }
     collide_check(player) {
-        let check = collision(player, key);
+        let check = collision(player, this);
         if(check == true) {
             this.held = true;
-            this.size = key.true_size/2;
+            this.size = this.true_size/2;
             this.x = player.x+player.size/4;
             this.y = player.y+player.size/4;
         } else {
@@ -106,16 +106,54 @@ class Key {
 class Door {
     open = false;
     true_size = 32;
+    size = 32;
     constructor(x,y) {
         this.x = x;
         this.y = y;
     }
     draw() {
-        ctx.fillStyle = 'brown';
-        ctx.beginPath();
-        ctx.rect(this.x,this.y,this.true_size,this.true_size)
-        ctx.closePath();
-        ctx.fill();
+        if(this.open == true) {
+            ctx.fillStyle = 'Silver';
+            ctx.beginPath();
+            ctx.rect(this.x,this.y,this.true_size,this.true_size);
+            ctx.closePath();
+            ctx.fill();
+    
+            ctx.fillStyle = 'Black';
+            ctx.beginPath();
+            ctx.rect(this.x+this.true_size/10,this.y+this.true_size/10,this.true_size-this.true_size/5,this.true_size-this.true_size/5);
+            ctx.closePath();
+            ctx.fill();
+        } else {
+            ctx.fillStyle = 'Silver';
+            ctx.beginPath();
+            ctx.rect(this.x,this.y,this.true_size,this.true_size);
+            ctx.closePath();
+            ctx.fill();
+    
+            ctx.fillStyle = 'Navy';
+            ctx.beginPath();
+            ctx.rect(this.x+this.true_size/10,this.y+this.true_size/10,this.true_size-this.true_size/5,this.true_size-this.true_size/5);
+            ctx.closePath();
+            ctx.fill();
+    
+            ctx.fillStyle = 'Silver';
+            ctx.beginPath();
+            ctx.rect(this.x+this.true_size/3,this.y+this.true_size/3,this.true_size/3,this.true_size/3);
+            ctx.rect(this.x,this.y+this.true_size/2.333,this.true_size,this.true_size/7);
+            ctx.rect(this.x+this.true_size/5.1,this.y,this.true_size/16,this.true_size);
+            ctx.rect(this.x+this.true_size/2.111,this.y,this.true_size/16,this.true_size);
+            ctx.rect(this.x+this.true_size/1.333,this.y,this.true_size/16,this.true_size);
+            ctx.closePath();
+            ctx.fill();
+    
+            ctx.fillStyle = 'Black';
+            ctx.beginPath();
+            ctx.rect(this.x+this.true_size/2.4,this.y+this.true_size/2.4,this.true_size/6,this.true_size/6);
+            ctx.closePath();
+            ctx.fill();
+        }
+
     }
 }
 
@@ -200,24 +238,39 @@ function update() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     boundary(player);
     
-    draw_fog();
+    // draw_fog();
+
+    // draw player door, and key will be drawn during keychecks
     player.draw();
+    door.draw();
 
     // checks to see if key is within player vision (kind of) and if the player is touching the key
     key.visible_check(player);
     key.collide_check(player, key);
-     // fog kind of works, but if the key is drawn before the fog, the key disappears.
+
+    // checks to see if key is not being held by the player and is touching a door.
+    // checks pass, the door will draw as if it is open
+    if(key.held == false && collision(key, door) == true) {
+        door.open = true;
+    } else {
+        door.open = false;
+    }
 }
 
+// set up canvas and context
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight-200;
 
+// drop some objects in the game screen
 var player = new Player(10,64,64,5,'red');
-var key = new Key(10, 88, 88);
+var key = new Key(10,88,88);
+var door = new Door(143,311);
 
+// keypress/keylift event handlers
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+// update the screen
 update();
